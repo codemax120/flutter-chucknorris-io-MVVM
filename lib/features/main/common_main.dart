03a6a6bb63b2e86a8dba9831678642c02a605155
Max Herrera <codemax120@gmail.com>
2023-02-11 20:11:18 -0600

    added main configuration project

---
+++ lib/features/main/common_main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  final String? initialRoute; // route from main.dart
+
+  const MyApp({
+    Key? key,
+    this.initialRoute = '/',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', 'US'),
+            Locale('es', 'ES'),
+          ],
+          locale: const Locale('es', 'ES'),
+          title: 'ChuckNorris',
+          initialRoute: initialRoute,
+          // onGenerateRoute: getNamedPage,
+        );
+      },
+    );
+  }
+}
