135467f9e8cc6ed2e04b5e9b22be3660d2f7e5b4
Max Herrera <codemax120@gmail.com>
2023-02-11 21:03:14 -0600

    added model for map json information in repositories layer

---
+++ lib/features/random/repositories/models/random_model.dart
@@ -0,0 +1,34 @@
+import 'package:chuck_norris_io/features/random/viewmodel/entities/random_entitie.dart';
+
+class RandomModel extends RandomEntity {
+  const RandomModel({
+    required super.id,
+    required super.url,
+    required super.value,
+    required super.iconUrl,
+    required super.createdAt,
+    required super.updatedAt,
+    required super.categories,
+  });
+
+  factory RandomModel.fromJson(Map<String, dynamic> json) {
+    List<String> categories = [];
+
+    if (json.containsKey('categories')) {
+      List<dynamic> inconmingCategories = json['categories'] as List;
+      for (var element in inconmingCategories) {
+        categories.add(element);
+      }
+    }
+
+    return RandomModel(
+      id: json['id'] ?? '',
+      url: json['url'] ?? '',
+      value: json['value'] ?? '',
+      iconUrl: json['icon_url'] ?? '',
+      createdAt: json['created_at'] ?? '',
+      updatedAt: json['updated_at'] ?? '',
+      categories: categories,
+    );
+  }
+}
