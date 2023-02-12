c6336776442ce411b3044d6fc0328ca73fbb59df
Max Herrera <codemax120@gmail.com>
2023-02-11 21:19:03 -0600

    added initFeaturesDependecies in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:chuck_norris_io/features/main/common_main.dart';
 import 'package:flutter/material.dart';
+import 'package:chuck_norris_io/features/main/common_main.dart';
+import 'package:chuck_norris_io/core/injection/injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  initDependecies();
+  await initFeaturesDependecies();
   runApp(const MyApp());
 }
