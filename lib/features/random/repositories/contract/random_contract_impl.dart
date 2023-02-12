f878ef2f9f9f7318f752ae5dc1346642c3b04992
Max Herrera <codemax120@gmail.com>
2023-02-11 21:05:54 -0600

    added random implements interface datasource for repositories layer

---
+++ lib/features/random/repositories/contract/random_contract_impl.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:chuck_norris_io/core/network/failure.dart';
+import 'package:chuck_norris_io/features/random/viewmodel/entities/random_entitie.dart';
+import 'package:chuck_norris_io/features/random/repositories/contract/random_contract.dart';
+import 'package:chuck_norris_io/features/random/repositories/datasources/random_datasource.dart';
+
+class RandomContractImpl implements RandomContract {
+  final RandomClient randomClient;
+
+  RandomContractImpl({
+    required this.randomClient,
+  });
+
+  @override
+  Future<Either<Failure, RandomEntity>> getRandom() async {
+    try {
+      final response = await randomClient.getRandom();
+      return Right(response);
+    } on Object {
+      return Left(ServerFailure());
+    }
+  }
+}
